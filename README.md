0d8e075e5e6e1bc692d8d9e42d90f61080b653b2
Erick Agrazal <erick.agrazal@gmail.com>
2018-11-24 02:12:33 -0500

    Added initial truffle configuration

--- README.md
+++ README.md
@@ -9,3 +9,23 @@ Un dapp para almacenar nicknames de un contrato inteligente
 2. `mkdir src`
 3. `cd src`
 4. `truffle init`  (Más información https://truffleframework.com/)
+
+**Segunda Etapa:**
+
+1. `truffle init`
+2. `truffle compile`
+3. `truffle migrate`
+4. Modifcar el archivo `truffle.js` para corregir el error `Error: No network specified. Cannot determine current network.`.
+```javascript
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+}; 
+```
+5. `docker-compose up`  # Levantar el ganache, nodo local de Ethereum.
+6. `truffle migrate`
--- initialize_server.sh
+++ initialize_server.sh
@@ -3,7 +3,7 @@
 set -x 
 
 function install_utilities(){
-    sudo apt-get install curl -y
+    sudo apt-get install build-essential curl -y
     curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
 }
 
@@ -11,8 +11,28 @@ function install_nodejs(){
     sudo apt-get install nodejs -y
 }
 
-function install_truffle(){
-    npm install -g truffle
+function install_npm_dependencies(){
+     sudo npm install -g truffle -y
+}
+
+function install_docker(){
+    echo 'Installing docker ...'
+    sudo add-apt-repository ppa:webupd8team/java -y
+    sudo apt-get -y update
+    sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key  fingerprint 0EBFCD88
+    sudo add-apt-repository  -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce
+    echo 'Finished docker install.'
+}
+
+function install_docker_compose(){
+    echo 'Installing docker-compose...'
+    sudo curl -L --fail https://github.com/docker/compose/releases/download/1.23.1/run.sh -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    echo 'Finished docker-compose install.'
 }
 
 echo '###################################'
@@ -21,7 +41,9 @@ echo '###################################'
 
 install_utilities
 install_nodejs
-install_truffle
+install_npm_dependencies
+install_docker
+install_docker_compose
 
 echo '###################################'
 echo 'Installation Complete.'
